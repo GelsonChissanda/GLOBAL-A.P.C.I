6c58616a573f77538cf1eaf37c1d8320775b10b7
Gelson Chissanda <gelsonchissanda@gmail.com>
2026-06-30 18:39:54 +0100

    logo no footer

--- gapci-website/src/components/Footer.jsx
+++ gapci-website/src/components/Footer.jsx
@@ -1,11 +1,17 @@
+import logo from "../assets/logoo.png"
+
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-400 px-6 py-12">
       <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-6">
         <div className="reveal text-center md:text-left">
-          <span className="text-2xl font-black text-white">
-            GAP<span className="text-orange-500">CI</span>
-          </span>
+          <img src={logo} alt="Logo GAPCI" className="h-14 w-14 object-contain" />
+                      <span
+                      className="text-sm  font-bold text-orange-600"
+                    >
+                      GLOBAL A.P.C.I
+                    </span>
+                    
           <p className="text-sm mt-1">Academia dos Profissionais de Ciências da Informação</p>
         </div>
 
